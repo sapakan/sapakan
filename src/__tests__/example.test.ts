7f514be1ab88b8051d801001d698f1c67bbb4d37
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2022-12-08 21:10:25 +0900

    Setup Jest, ESLint, Prettier, Prisma

---
+++ src/__tests__/example.test.ts
@@ -0,0 +1,4 @@
+// TODO: delete this file and write the actual tests
+test("example", () => {
+  console.log("hello, world");
+});
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// TODO: delete this file and write the actual implementation
+console.log("Hello World!");
