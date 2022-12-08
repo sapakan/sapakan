99f4fc3e2ac05bf3c4d2ad8498a0f799e3e33e6c
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2022-12-08 23:17:58 +0900

    Register routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import bodyParser from "body-parser";
 import express from "express";
 
 import * as homeController from "./controllers/home";
+import * as postsController from "./controllers/posts";
+import * as accountsController from "./controllers/accounts";
 
 const app = express();
 
@@ -10,5 +12,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", homeController.index);
+app.post("/posts", postsController.postPosts);
+app.get("/posts/:id", postsController.getPost);
+app.get("/posts/:id/likes", postsController.getPostLikes);
+app.post("/posts/:id/likes", postsController.postPostLikes);
+app.delete("/posts/:id/likes", postsController.deletePostLikes);
+app.post("/accounts", accountsController.postAccounts);
+app.get("/accounts/:id", accountsController.getAccount);
+app.get("/accounts/:id/posts", accountsController.getAccountPosts);
+app.get("/accounts/:id/likes", accountsController.getAccountLikes);
 
 export default app;
