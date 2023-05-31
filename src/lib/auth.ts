0e68c8663bf6bc5c5a577a3fd259acc0c3041955
sobadon <sbdn@chasoba.net>
2023-05-31 07:49:44 +0000

    鍵長に関するコメントを追加

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -25,6 +25,11 @@ export function verifyPassword(hash: string, password: string) {
 /**
  * 長さ 2048 の RSA 鍵ペアを生成します。
  * [publicKey, privateKey] を返します。
+ *
+ * 長さは多数派の 2048 にしている。
+ * Pleroma は 2048: https://git.pleroma.social/pleroma/pleroma/-/blob/31ec5cd35eece97aa1213c401b40d3ab83689ea9/lib/pleroma/keys.ex#L11
+ * Mastodon は 2048: https://github.com/mastodon/mastodon/blob/55785b160320783392ffe3f24c5ca48e6ee7a5f2/app/models/account.rb#L465-L471
+ * Misskey は 4096: https://github.com/misskey-dev/misskey/blob/6dd219b6c7d13d6852a5e4173fb8cd7430bd41ff/packages/backend/src/core/SignupService.ts#L93-L108
  */
 export async function createKeyPair(): Promise<[string, string]> {
   // 長さ 2048 の RSA 鍵を生成する
