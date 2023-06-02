fdeae4353a0100d8a7cffaff1f7707d55b9a54b1
sobadon <sbdn@chasoba.net>
2023-06-02 06:06:29 +0000

    fix lint error

--- test/src-lib/follower.test.ts
+++ test/src-lib/follower.test.ts
@@ -1,7 +1,11 @@
 import { listRemoteFollowerAccounts } from "../../src/lib/follower";
 import { createExternalAccount } from "../../src/services/create-account";
 import { createLocalAccount } from "../../src/services/create-account";
-import { accountFactory, externalAccountFactory, followingFactory } from "../lib/factories";
+import {
+  accountFactory,
+  externalAccountFactory,
+  followingFactory,
+} from "../lib/factories";
 
 describe(listRemoteFollowerAccounts, () => {
   test("自身のフォロワーで、かつリモートのアカウント一覧を取得できる", async () => {
@@ -9,8 +13,14 @@ describe(listRemoteFollowerAccounts, () => {
     const followerLocal = await accountFactory.create();
     const followerRemote = await externalAccountFactory.create();
 
-    await followingFactory.create({ followeeId: me.id, followerId: followerLocal.id });
-    await followingFactory.create({ followeeId: me.id, followerId: followerRemote.id });
+    await followingFactory.create({
+      followeeId: me.id,
+      followerId: followerLocal.id,
+    });
+    await followingFactory.create({
+      followeeId: me.id,
+      followerId: followerRemote.id,
+    });
 
     const remoteFollowers = await listRemoteFollowerAccounts(me.id);
 
@@ -25,7 +35,7 @@ describe(listRemoteFollowerAccounts, () => {
     expect(remoteFollowers).toStrictEqual(
       expect.not.objectContaining({
         id: followerLocal.id,
-      }),
+      })
     );
   });
 });
